c82d3b3dd20e3a92990213513a2de6c313eb0fbb
claytongentry <claytongentry2017@u.northwestern.edu>
2017-07-30 19:49:15 -0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Furlex.Mixfile do
   def project do
     [
       app: :furlex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
