66753896ad6f738b682cd41744b0ee5289e6ccff
amy null <null@amy.gg>
2021-02-20 16:30:27 -0500

    Formatting and dependency updates

--- lib/furlex/oembed.ex
+++ lib/furlex/oembed.ex
@@ -16,22 +16,24 @@ defmodule Furlex.Oembed do
   Soft fetch will fetch cached providers. Hard fetch requests
   providers from oembed.com and purges the cache.
   """
-  @spec fetch_providers(Atom.t) :: {:ok, List.t} | {:error, Atom.t}
+  @spec fetch_providers(Atom.t()) :: {:ok, List.t()} | {:error, Atom.t()}
   def fetch_providers(type \\ :soft)
+
   def fetch_providers(:hard) do
     case get("/providers.json") do
       {:ok, %{body: providers}} ->
-        GenServer.cast __MODULE__, {:providers, providers}
+        GenServer.cast(__MODULE__, {:providers, providers})
         {:ok, providers}
 
-      other                ->
-        Logger.error "Could not fetch providers: #{inspect other}"
+      other ->
+        Logger.error("Could not fetch providers: #{inspect(other)}")
         {:error, :fetch_error}
     end
   end
+
   def fetch_providers(_soft) do
     case GenServer.call(__MODULE__, :providers) do
-      nil       -> fetch_providers(:hard)
+      nil -> fetch_providers(:hard)
       providers -> {:ok, providers}
     end
   end
@@ -47,10 +49,10 @@ defmodule Furlex.Oembed do
     iex> Oembed.endpoint_from_url "https://vimeo.com/88856141", %{"format" => "xml"}
     {:ok, "https://vimeo.com/api/oembed.xml"}
   """
-  @spec endpoint_from_url(String.t, Map.t) :: {:ok, String.t} | {:error, Atom.t}
+  @spec endpoint_from_url(String.t(), Map.t()) :: {:ok, String.t()} | {:error, Atom.t()}
   def endpoint_from_url(url, params \\ %{"format" => "json"}, opts \\ []) do
     case provider_from_url(url, opts) do
-      nil      ->
+      nil ->
         {:error, :no_oembed_provider}
 
       provider ->
@@ -60,39 +62,38 @@ defmodule Furlex.Oembed do
 
   # Maps a url to a provider, or returns nil if no such provider exists
   defp provider_from_url(url, opts) do
-    fetch_type =
-      if Keyword.get(opts, :skip_cache?, false), do: :hard, else: :soft
+    fetch_type = if Keyword.get(opts, :skip_cache?, false), do: :hard, else: :soft
 
     {:ok, providers} = fetch_providers(fetch_type)
 
     case URI.parse(url) do
-      %URI{host: nil}  ->
+      %URI{host: nil} ->
         nil
 
       %URI{host: host} ->
-        Enum.find providers, &host_matches?(host, &1)
+        Enum.find(providers, &host_matches?(host, &1))
     end
   end
 
   defp endpoint_from_provider(provider, params) do
-    [ endpoint | _] = provider["endpoints"]
+    [endpoint | _] = provider["endpoints"]
 
-    url   = endpoint["url"]
+    url = endpoint["url"]
     regex = ~r/{(.*?)}/
-    url   = Regex.replace regex, url, fn _, key -> params[key] end
+    url = Regex.replace(regex, url, fn _, key -> params[key] end)
 
     {:ok, url}
   end
 
   defp host_matches?(host, %{"provider_url" => provider_url}) do
-    Regex.match? ~r/https?:\/\/#{host}/, provider_url
+    Regex.match?(~r/https?:\/\/#{host}/, provider_url)
   end
 
   ## GenServer callbacks
 
   @doc false
-  def start_link(opts \\ []) do
-    GenServer.start_link __MODULE__, nil, opts
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
   def init(state) do
--- lib/furlex/parser/html.ex
+++ lib/furlex/parser/html.ex
@@ -3,10 +3,16 @@ defmodule Furlex.Parser.HTML do
 
   alias Furlex.Parser.{Facebook, Twitter}
 
-  @spec parse(String.t) :: nil | {:ok, Map.t}
+  @spec parse(String.t()) :: nil | {:ok, Map.t()}
   def parse(html) do
-    case Floki.find(html, "meta[name]") do
-      nil      -> {:ok, %{}}
+    html
+    |> Floki.parse_document()
+    |> elem(1)
+    |> Floki.find("meta[name]")
+    |> case do
+      nil ->
+        {:ok, %{}}
+
       elements ->
         content =
           elements
@@ -19,16 +25,18 @@ defmodule Furlex.Parser.HTML do
 
   # Filter out plain meta elements from Twitter, Facebook, etc.
   defp filter_other(elements) do
-    Enum.reject elements, fn element ->
-       extract_attribute(element, "name") in (Facebook.tags ++ Twitter.tags)
-    end
+    Enum.reject(elements, fn element ->
+      extract_attribute(element, "name") in (Facebook.tags() ++ Twitter.tags())
+    end)
   end
 
   defp to_map(element, acc) do
-    key      = extract_attribute(element, "name")
+    key = extract_attribute(element, "name")
     existing = Map.get(acc, key)
-    to_add   = extract_attribute(element, "content") ||
-               extract_attribute(element, "property")
+
+    to_add =
+      extract_attribute(element, "content") ||
+        extract_attribute(element, "property")
 
     if is_nil(existing) do
       Map.put(acc, key, to_add)
@@ -38,8 +46,8 @@ defmodule Furlex.Parser.HTML do
         |> prepend(existing)
         |> Enum.uniq()
         |> case do
-          [ element ] -> element
-          list        -> list
+          [element] -> element
+          list -> list
         end
 
       Map.put(acc, key, value)
@@ -48,11 +56,11 @@ defmodule Furlex.Parser.HTML do
 
   defp extract_attribute(element, key) do
     case Floki.attribute(element, key) do
-      [ attribute ] -> attribute
-      _             -> nil
+      [attribute] -> attribute
+      _ -> nil
     end
   end
 
-  defp prepend(value, list) when is_list(list), do: [ value | list ]
-  defp prepend(value, element),                 do: [ value | [ element ]]
+  defp prepend(value, list) when is_list(list), do: [value | list]
+  defp prepend(value, element), do: [value | [element]]
 end
